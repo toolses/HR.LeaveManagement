864656ff453ebcc0706a82faf4344427cbb20018
toolses <toolses.22@gmail.com>
2022-05-18 11:25:05 +0200

    Section 3-9

--- Hr.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
+++ Hr.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -10,5 +10,9 @@ namespace Hr.LeaveManagement.Application.Persistence.Contracts
     {
         Task<T> Get(int id);
         Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+
     }
 }
---
+++ Hr.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Hr.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
