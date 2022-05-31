e17697d3f1d0cd89502e8f9ff35567c64d80dbfe
toolses <toolses.22@gmail.com>
2022-05-31 15:47:01 +0200

    Section 4-19 | Implementing Persistence Layer

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,37 @@
+﻿using Hr.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+
+        public LeaveAllocationRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails()
+        {
+            var leaveAlloctions = await _dbContext.LeaveAllocations
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+            return leaveAlloctions;
+        }
+
+        public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+        {
+            var leaveAllocation = await _dbContext.LeaveAllocations
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q => q.Id == id);
+            return leaveAllocation;
+        }
+    }
+}
--- Hr.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
+++ Hr.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -12,8 +12,8 @@ namespace Hr.LeaveManagement.Application.Persistence.Contracts
         Task<IReadOnlyList<T>> GetAll();
         Task<bool> Exists(int id);
         Task<T> Add(T entity);
-        Task<T> Update(T entity);
-        Task<T> Delete(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
 
     }
 }
