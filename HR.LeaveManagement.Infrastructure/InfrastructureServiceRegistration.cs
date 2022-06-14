dbfa83299e193fee9532503c93b869d3376b14fd
toolses <toolses.22@gmail.com>
2022-06-14 15:34:21 +0200

    Section 4-20 | Add Infrastructure Project (Email Service)

---
+++ HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Hr.LeaveManagement.Application.Contracts.Infrastructure;
+using Hr.LeaveManagement.Application.Models;
+using HR.LeaveManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection  ConfigureInfrastructureServices (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
---
+++ Hr.LeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Hr.LeaveManagement.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
